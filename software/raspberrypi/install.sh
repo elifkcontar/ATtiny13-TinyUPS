82edcf455b4cc8d55e8090e134b598f353b7e5fc
Elif K Çontar <elifkcontar@gmail.com>
2022-03-25 15:50:23 +0300

    Update install.sh
    +Folder paths are changed

--- software/raspberrypi/install.sh
+++ software/raspberrypi/install.sh
@@ -11,9 +11,9 @@ echo "Create folder /opt/tinyUPS ..."
 sudo mkdir /opt/tinyUPS
 
 echo "Copy scripts to /opt/tinyUPS ..."
-sudo cp tinyUPSshutdown.py /opt/tinyUPS
-sudo cp tinyUPSrequest.py /opt/tinyUPS
-sudo cp tinyUPSshutdown.sh /opt/tinyUPS
+sudo cp /home/pi/System/tinyUPSshutdown.py /opt/tinyUPS
+sudo cp /home/pi/System/tinyUPSrequest.py /opt/tinyUPS
+sudo cp /home/pi/System/tinyUPSshutdown.sh /opt/tinyUPS
 
 echo "Make scripts executable ..."
 sudo chmod +rx /opt/tinyUPS/tinyUPSshutdown.py
@@ -21,7 +21,7 @@ sudo chmod +rx /opt/tinyUPS/tinyUPSrequest.py
 sudo chmod +rx /opt/tinyUPS/tinyUPSshutdown.sh
 
 echo "Copy tinyUPS.service to /etc/systemd/system ..."
-sudo cp tinyUPS.service /etc/systemd/system
+sudo cp /home/pi/System/tinyUPS.service /etc/systemd/system
 
 echo "Enable tinyUPS.service ..."
 sudo systemctl enable tinyUPS.service
